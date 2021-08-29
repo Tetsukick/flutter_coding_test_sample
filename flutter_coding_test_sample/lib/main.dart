5b4a724912e97fbc94ca983ae772abcb0d99fd08
AQ-TeppeiKikuchi <teppei.kikuchi@asia-quest.co.jp>
2021-08-30 01:59:03 +0900

    add carousel components

--- flutter_coding_test_sample/lib/main.dart
+++ flutter_coding_test_sample/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_coding_test_sample/components/carousel.dart';
 
 void main() {
   runApp(MyApp());
@@ -46,6 +47,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Carousel(),
             Text(
               'You have pushed the button this many times:',
             ),
